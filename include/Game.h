79f25b63d6f9801e7f0118f8013edcd6d76376ea
ianaldosg <ianaldosg@gmail.com>
2026-04-02 15:40:04 -0300

    GetInstance () : Game&

--- include/Game.h
+++ include/Game.h
@@ -1,2 +1,14 @@
 #define INCLUDE_SDL
+
 #include "SDL_include.h"
+#include <iostream>
+#include <string>
+
+class Game {
+private:
+    Game();
+    static Game* instance;
+
+public:
+    static Game& GetInstance(); 
+};
--- src/Game.cpp
+++ src/Game.cpp
@@ -2,3 +2,16 @@
 #define INCLUDE_SDL_MIXER
 
 #include "../include/Game.h"
+
+Game* Game::instance = nullptr;
+
+Game::Game() {
+    std::cout << "Jogo inicializado." << std::endl;
+}
+Game& Game::GetInstance(){
+    if (instance == nullptr){
+        instance = new Game();
+    }
+    return *instance;
+}
+
