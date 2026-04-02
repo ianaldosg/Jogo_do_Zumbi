b90f582bbcb5f91d8d8d172d16e731a0d2492317
ianaldosg <ianaldosg@gmail.com>
2026-04-02 17:54:15 -0300

    Finalização_da_Estrutura_Básica

--- include/Game.h
+++ include/Game.h
@@ -1,14 +1,22 @@
 #define INCLUDE_SDL
 
 #include "SDL_include.h"
+#include "State.h"
 #include <iostream>
 #include <string>
 
 class Game {
 private:
-    Game();
+    Game(std::string title, int width, int height);
     static Game* instance;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    State* state;
 
 public:
-    static Game& GetInstance(); 
+    static Game& GetInstance(std::string title, int width, int height); 
+    ~Game();
+    State& GetState();
+    SDL_Renderer* GetRenderer();
+    void Run();
 };
--- src/Game.cpp
+++ src/Game.cpp
@@ -5,13 +5,100 @@
 
 Game* Game::instance = nullptr;
 
-Game::Game() {
-    std::cout << "Jogo inicializado." << std::endl;
+Game::Game(std::string title, int width, int height) {
+    if (instance != nullptr){
+        std::cout << "Erro: Game já instaciado!" << std::endl;
+        exit(1);
+    }
+
+    instance = this;
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
+        std::cout << "Erro SDL_Init: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+
+    int imgFlags = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF;
+
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+        std::cout << "Erro IMG_Init: " << IMG_GetError() << std::endl;
+        exit(1);
+    }
+
+    int mixFlags = MIX_INIT_MP3 | MIX_INIT_OGG | MIX_INIT_MOD;
+    if (!(Mix_Init(mixFlags) & mixFlags)) {
+        std::cout << "Erro Mix_Init: " << Mix_GetError() << std::endl;
+        exit(1);
+    }
+    if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024) != 0) {
+        std::cout << "Erro Mix_OpenAudio: " << Mix_GetError() << std::endl;
+        exit(1);
+    }
+    Mix_AllocateChannels(32);
+
+    window = SDL_CreateWindow(
+        title.c_str(), 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        width, 
+        height, 
+        0
+    );
+    if (window == nullptr) {
+        std::cout << "Erro SDL_CreateWindow: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+
+    renderer = SDL_CreateRenderer(
+        window, 
+        -1, 
+        SDL_RENDERER_ACCELERATED
+    );
+    if (renderer == nullptr) {
+        std::cout << "Erro SDL_CreateRenderer: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+
 }
-Game& Game::GetInstance(){
+
+Game::~Game(){
+    if (renderer != nullptr) {
+        SDL_DestroyRenderer(renderer);
+        renderer = nullptr;
+    }
+
+    if (window != nullptr) {
+        SDL_DestroyWindow(window);
+        window = nullptr;
+    }
+
+    Mix_CloseAudio();
+
+    Mix_Quit();
+
+    IMG_Quit();
+
+    SDL_Quit();
+
+    std::cout << "Jogo Encerrado!" << std::endl;
+}
+
+Game& Game::GetInstance(std::string title, int width, int height){
     if (instance == nullptr){
-        instance = new Game();
+        instance = new Game(title, width, height);
     }
     return *instance;
+}
+
+State& Game::GetState(){
+    return *state;
+}
+
+SDL_Renderer* Game::GetRenderer() {
+    return renderer;
+}
+
+void Game::Run(){
+    std::cout << "Rodando..." << std::endl;
 }
 
