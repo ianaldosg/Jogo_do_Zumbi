89a57e27527fc731c1baf0c106ebb66c29604734
ianaldosg <ianaldosg@gmail.com>
2026-04-16 11:31:46 -0300

    Etapa 1 do Trabalho 2 (90% completa)

--- include/Vec2.h
+++ include/Vec2.h
@@ -7,10 +7,18 @@ public:
     Vec2();
     Vec2(float x, float y);
 
-    Vec2 operator+(const Vec2& v);
-    Vec2 operator-(const Vec2& v);
-    Vec2 operator*(float esacalar);
+    Vec2 operator+(const Vec2& v) const;
+    Vec2 operator-(const Vec2& v) const;
+    Vec2 operator*(float esacalar) const;
 
-    float Magnitude();
-    Vec2 Normalizar();
+    float Magnitude() const;
+
+    Vec2 Normalizar() const;
+
+    float Distancia(const Vec2& v) const;
+
+    float Angulo() const;
+    float Angulo(const Vec2& v) const;
+
+    Vec2 Rotacao(float angulo) const;
 };
--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -5,24 +5,47 @@ Vec2::Vec2() : x(0), y(0){}
 
 Vec2::Vec2(float x, float y): x(x), y(y) {}
 
-Vec2 Vec2::operator+(const Vec2& v){
+Vec2 Vec2::operator+(const Vec2& v) const{
     return Vec2(x + v.x, y + v.y);
 }
 
-Vec2 Vec2::operator-(const Vec2& v){
+Vec2 Vec2::operator-(const Vec2& v) const{
     return Vec2(x - v.x, y - v.y);
 }
 
-Vec2 Vec2::operator*(float escalar){
+Vec2 Vec2::operator*(float escalar) const{
     return Vec2(x * escalar, y * escalar);
 }
 
-float Vec2::Magnitude(){
+float Vec2::Magnitude() const{
     return std::sqrt(x*x + y*y);
 }
 
-Vec2 Vec2::Normalizar() {
+Vec2 Vec2::Normalizar() const {
     float mag = Magnitude();
     if (mag == 0) return Vec2(0,0);
     return Vec2(x/mag, y/mag);
+}
+
+float Vec2::Distancia(const Vec2& v) const{
+    return (*this - v).Magnitude();
+}
+
+float Vec2::Angulo() const{
+    return atan2(y, x);
+}
+
+float Vec2::Angulo(const Vec2& v) const{
+    Vec2 diferença = v - *this;
+    return atan2(diferença.y, diferença.x);
+}
+
+Vec2 Vec2::Rotacao(float angulo) const{
+    float cosA = cos(angulo);
+    float sinA = sin(angulo);
+
+    return Vec2(
+            x * cosA - y * sinA,
+            y * cosA + x * sinA
+            );
 }
