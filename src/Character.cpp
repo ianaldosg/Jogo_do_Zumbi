f8145ff8e61ec75fbb56cfaa3be45c2726eb7740
ianaldosg <ianaldosg@gmail.com>
2026-05-27 22:46:56 -0300

    Inicio da Etapa 2 Construtores de Gun e Character, Trabalho 5

--- src/Character.cpp
+++ src/Character.cpp
@@ -1,21 +1,43 @@
 #include "../include/Character.h"
 #include "../include/SpriteRenderer.h"
 #include "../include/Animator.h"
+#include "../include/Gun.h"
+#include "../include/Game.h"
 
 Character* Character::player = nullptr;
 
-Character::Character(GameObject& associated, std::string sprite) : Component(associated){
+Character::Character(GameObject& associated, std::string sprite) : Component(associated), 
+    hp(100), 
+    linearSpeed(100.0f) {
 
-    hp = 10;
-    linearSpeed = 100.0f;
     speed = Vec2(0, 0);
 
-    associated.AddComponent(new SpriteRenderer(associated, sprite));
+    //associated.AddComponent(new SpriteRenderer(associated, sprite));
+    SpriteRenderer* sr = new SpriteRenderer(
+            associated,
+            "Recursos/img/Player.png",
+            3, 4
+            );
+
+    associated.AddComponent(sr);
 
     Animator* anim = new Animator(associated);
 
-    anim->AddAnimation("idle", );
-    anim->AddAnimation("walking", );
-    anim->AddAnimation("dead", );
+    anim->AddAnimation("idle", Animation(6, 7, 0.2f));
+    anim->AddAnimation("walking", Animation(0, 5, 0.2f));
+    anim->AddAnimation("dead", Animation(10, 11, 0.2f));
+    associated.AddComponent(anim);
+}
 
+Character::~Character() {
+    if (player == this) {
+        player = nullptr;
+    }
+}
+
+void Character::Start() {
+    GameObject* gunGo = new GameObject();
+    gunGo->AddComponent(new Gun(*gunGo, Game::GetInstance().GetState().GameObjectPtr(&associated)));
+
+    gun = Game::GetInstance().GetState().AddObject(gunGo);
 }
--- src/Gun.cpp
+++ src/Gun.cpp
@@ -1,1 +1,19 @@
 #include "../include/Gun.h"
+#include "../include/SpriteRenderer.h"
+#include "../include/Animator.h"
+
+Gun::Gun(GameObject& associated, std::weak_ptr<GameObject> character) : Component(associated),
+    shotSound("Recursos/audio/Range.wav"),
+    reloadSound("Recursos/audio/PumpAction.mp3"),
+    cooldownState(0),
+    angle(0),
+    character(character) {
+
+        associated.AddComponent(new SpriteRenderer(associated, "Recursos/img/Gun.png", 3,2));
+        
+        Animator* anim = new Animator(associated);
+        anim->AddAnimation("idle", Animation(0,0,0));
+        anim->AddAnimation("reloading", Animation(1,5,0.2f));
+        associated.AddComponent(anim);
+
+    }
